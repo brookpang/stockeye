186c48487d06ff22bf01369ed3f556f3665e7a71
pang <pangyexing@qfpay.com>
2016-07-03 19:57:51 +0800

    修改bug

--- db/db.js
+++ db/db.js
@@ -1,33 +1,38 @@
 var pg = require('pg');
 
-// create a config to configure both pooling behavior
-// and client options
-// note: all config is optional and the environment variables
-// will be read if the config is not present
 var config = require('../conf/conf.js')
+var Pool = pg.Pool;
+var pool = new Pool(config)
 
 
-
-var client = new pg.Client(config);
-client.on('drain', client.end.bind(client)); //disconnect client when all queries are finished
-client.connect();
-
-var _select = function(sqlstring,callback ) {
-   var cq= client.query(
-      sqlstring
-      ,
-        function(err, result) {
-            // console.log(result.rows[0].name) // output: brianc
-            if (err) {
-                console.error('error running query', err);
-            }
-          callback(result);
-    }
-  );
-  // cq.on('row', function(row) {
-  //   // assert.equal("brian@example.com", row.email);
-  //   callback(row);
-  // });
+var _select = function(sqlstring, callback) {
+    pool.query(sqlstring, function(err, result) {
+        // handle an error from the query
+        if (err) return onError(err);
+      callback(result);
+    });
 };
+
+// var client = new pg.Client(config);
+// client.on('drain', client.end.bind(client)); //disconnect client when all queries are finished
+// client.connect();
+
+// var _select = function(sqlstring,callback ) {
+//    var cq= client.query(
+//       sqlstring
+//       ,
+//         function(err, result) {
+//             // console.log(result.rows[0].name) // output: brianc
+//             if (err) {
+//                 console.error('error running query', err);
+//             }
+//           callback(result);
+//     }
+//   );
+//   // cq.on('row', function(row) {
+//   //   // assert.equal("brian@example.com", row.email);
+//   //   callback(row);
+//   // });
+// };
 
 module.exports.dbselect = _select;
