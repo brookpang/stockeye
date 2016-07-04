673af2c79f1c19bc9278dcc6974f3ec77c71fd2b
pang <pangyexing@qfpay.com>
2016-07-04 09:35:05 +0800

    新增历史图表模版

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var recommend = require('./routes/recommend');
+var acc = require('./routes/acc');
 
 var app = express();
 
@@ -26,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/recommend', recommend);
+app.use('/acc', acc);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ routes/acc.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+// var dbselect = require('../db/db').dbselect;
+
+// /* GET recommend page. */
+// var sqlrecommend = 'select c_yearmonthday,code,name,round(predict,4) as predict from predict_head order by c_yearmonthday desc limit 2';
+// var sqlstatics = 'select round(p_mean,4) p_mean,round(p_std,4) p_std,round(p_min,4) p_min,round(p25,4) p25,round(p50,4) p50,round(p75,4) p75,round(p_max,4) p_max from predict_statics order by c_yearmonthday desc limit 1';
+// var stas = '';
+// var rec = '';
+// dbselect(sqlstatics, function(result) {
+//     stas = result.rows[0];
+// });
+// dbselect(sqlrecommend, function(result) {
+//     rec = result.rows;
+// });
+
+router.get('/', function(req, res, next) {
+    res.render('acc', {
+        stas: '',
+        rec:''
+    });
+});
+
+module.exports = router;
