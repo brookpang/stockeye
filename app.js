0276104219f27d9ff14e49a5fbcecf259041cd1d
pang <pangyexing@qfpay.com>
2016-07-04 16:37:35 +0800

    新增历史推荐信息

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var recommend = require('./routes/recommend');
+var recommendh = require('./routes/recommendh');
 var acc = require('./routes/acc');
 
 var app = express();
@@ -27,6 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/recommend', recommend);
+app.use('/recommendh', recommendh);
 app.use('/acc', acc);
 
 // catch 404 and forward to error handler
---
+++ routes/recommendh.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+var dbselect = require('../db/db').dbselect;
+
+/* GET recommend page. */
+var sqlrecommendh = 'select c_yearmonthday,code,name,round(predict,4) as predict,round(p_change,4) p_change,round(acc,4)  acc from acc1 order by c_yearmonthday desc,predict desc limit 20';
+var res_h = '';
+dbselect(sqlrecommendh, function(result) {
+    res_h = result.rows;
+});
+
+router.get('/', function(req, res, next) {
+    res.render('recommendh', {
+        res_h: res_h
+    });
+});
+
+module.exports = router;
