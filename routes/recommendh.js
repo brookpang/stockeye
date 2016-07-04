6400d343291e081956d9dc2dff37ed5c77f9034c
pang <pangyexing@qfpay.com>
2016-07-04 17:17:30 +0800

    修改日期去掉年

--- routes/recommendh.js
+++ routes/recommendh.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var dbselect = require('../db/db').dbselect;
 
 /* GET recommend page. */
-var sqlrecommendh = 'select c_yearmonthday,code,name,round(predict,4) as predict,round(p_change,4) p_change,round(acc,4)  acc from acc1 order by c_yearmonthday desc,predict desc limit 20';
+var sqlrecommendh = 'select substr(c_yearmonthday,5,5) as c_yearmonthday,code,name,round(predict,4) as predict,round(p_change,4) p_change,round(acc,4)  acc from acc1 order by c_yearmonthday desc,predict desc limit 20';
 var res_h = '';
 dbselect(sqlrecommendh, function(result) {
     res_h = result.rows;
