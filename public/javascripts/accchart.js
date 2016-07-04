c388e51feaf618ea47f82a884da2c7639dcff1d7
pang <pangyexing@qfpay.com>
2016-07-05 06:43:25 +0800

    新增acc图表

---
+++ public/javascripts/accchart.js
@@ -0,0 +1,77 @@
+option = {
+    tooltip: {
+        trigger: 'axis'
+    },
+    legend: {
+        data: ['收益率', '准确率']
+    },
+    toolbox: {
+        show: false,
+        feature: {
+            dataZoom: {
+                yAxisIndex: 'none'
+            },
+            dataView: {
+                readOnly: false
+            },
+            magicType: {
+                type: ['line', 'bar']
+            },
+            restore: {},
+            saveAsImage: {}
+        }
+    },
+    xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: []
+    },
+    yAxis: {
+        type: 'value',
+        axisLabel: {
+            formatter: '{value}'
+        }
+    },
+    series: [{
+        name: '收益率',
+        type: 'line',
+        data: [],
+        markPoint: {
+            data: [{
+                type: 'max',
+                name: '最大值'
+            }, {
+                type: 'min',
+                name: '最小值'
+            }]
+        },
+        markLine: {
+            data: [{
+                type: 'average',
+                name: '平均值'
+            }]
+        }
+    }, {
+        name: '准确率',
+        type: 'line',
+        data: [],
+        markPoint: {
+            data: [{
+                type: 'max',
+                name: '最大值'
+            }, {
+                type: 'min',
+                name: '最小值'
+            }]
+        },
+        markLine: {
+            data: [{
+                type: 'average',
+                name: '平均值'
+            }]
+        }
+    }]
+};
+
+
+
--- routes/acc.js
+++ routes/acc.js
@@ -2,15 +2,34 @@ var express = require('express');
 var router = express.Router();
 var dbselect = require('../db/db').dbselect;
 
-var sqlaccstatics = 'select  * from (select c_yearmonthday,round(h_p_change,4) h_p_change,round(h_p_acc,4) h_p_acc from acc2 order by c_yearmonthday desc limit 1) a,(select round(max(h_p_acc),4) as max_h_p_acc,round(min(h_p_acc),4) as min_h_p_acc,round(max(h_p_change),4) as max_h_p_change,round(min(h_p_change),4) as min_h_p_change from acc2) b';
+
+var sqlaccstatics = 'select  * from (select c_yearmonthday,round(h_p_change,3) h_p_change,round(h_p_acc,3) h_p_acc from acc2 order by c_yearmonthday desc limit 1) a,(select round(max(h_p_acc),3) as max_h_p_acc,round(min(h_p_acc),3) as min_h_p_acc,round(max(h_p_change),3) as max_h_p_change,round(min(h_p_change),3) as min_h_p_change from acc2) b';
 var accstas = '';
 dbselect(sqlaccstatics, function(result) {
     accstas = result.rows[0];
 });
-
+var sqlacc = 'select c_yearmonthday,round(h_p_acc,3) h_p_acc,round(h_p_change,3) h_p_change from acc2 order by c_yearmonthday';
+// var c_yearmonthday = new Array();
+var h_p_change = new Array();
+var h_p_acc = new Array();
+var c_yearmonthday = new Array();
+dbselect(sqlacc, function(result) {
+    var accrows = result.rows;
+    for (var i = 0; i < accrows.length; i++) {
+        c_yearmonthday = new Array();
+        h_p_change = new Array();
+        h_p_acc = new Array();
+        c_yearmonthday.push(accrows[i].c_yearmonthday);
+        h_p_change.push(accrows[i].h_p_change);
+        h_p_acc.push(accrows[i].h_p_acc);
+    }
+});
 router.get('/', function(req, res, next) {
     res.render('acc', {
-        accstas: accstas
+        accstas: accstas,
+      c_yearmonthday: c_yearmonthday,
+      h_p_acc: h_p_acc,
+      h_p_change:h_p_change
     });
 });
 
